6273831f7150ea19695f36ce88a4c143e0ffcbbd
pchom <pchom@hotmail.com>
2026-03-30 21:18:24 +0800

    feat: remove ARM toolchain and STM32 packages of xmake

--- packages/c/cmsis/cmsis.lua
+++
@@ -1,8 +0,0 @@
-set_defaultplat("corss")
-
-target("cmsis", function()
-    set_kind("headeronly")
-
-    add_includedirs("CMSIS/Core/Include", { public = true })
-    add_headerfiles("CMSIS/Core/Include/(**.h)")
-end)
